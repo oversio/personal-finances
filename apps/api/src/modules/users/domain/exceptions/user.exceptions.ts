d1cfd0f0412d638019f2f6de4df11d4809fffd1d
Over Martinez <overmartinez@gmail.com>
2026-02-05 22:48:57 +0100

    refactor(api): remove unneccesary lint rule disabling

--- apps/api/src/modules/users/domain/exceptions/user.exceptions.ts
+++ apps/api/src/modules/users/domain/exceptions/user.exceptions.ts
@@ -3,7 +3,6 @@ import { DomainException, ErrorCodes } from "@/modules/shared/domain/exceptions"
 export class UserNotFoundError extends DomainException {
   constructor(identifier: string) {
     super(`User not found: ${identifier}`, {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
       errorCode: ErrorCodes.users.notFound,
       fieldName: null,
       handler: "user",
@@ -14,7 +13,6 @@ export class UserNotFoundError extends DomainException {
 export class UserAlreadyExistsError extends DomainException {
   constructor(email: string) {
     super(`User with email ${email} already exists`, {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-assignment
       errorCode: ErrorCodes.auth.emailAlreadyExists,
       fieldName: "email",
       handler: "user",
